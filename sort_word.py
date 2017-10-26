b631696f71f82b8e6d44c08b6b48060417c08aed
tim-wyatt <33131948+tim-wyatt@users.noreply.github.com>
2017-10-26 21:31:49 +0100

    readding sort_word.py

--- sort_word.py
+++ sort_word.py
@@ -1,11 +1,21 @@
+
 """The aim of this project is to take a file of text and sort the words based on the last 
 letter of each word. This is the main entry point"""
 
 import sys
 
 def main ():
-	"""main entry point for the programme"""
-	pass
+	
+	"""first thing we need to do is read in a file that is passed in as the programme is
+	executed"""
+
+
+def parsetext ():
+
+
+"""I want to use a linked list to store the words so that I can easily insert stuff into
+into the middle of the list"""	
+
 
 
 __name__ == '__main__':
