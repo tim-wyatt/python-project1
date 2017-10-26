aacc7172a95ed5b00ca41af643468744dea8b48a
tim-wyatt <33131948+tim-wyatt@users.noreply.github.com>
2017-10-26 23:31:03 +0100

    changes to wordcharsstruc

--- wordcharsstruc.py
+++ wordcharsstruc.py
@@ -2,13 +2,13 @@
 
 class wordStruct:
 
-"""This will be used to store data about each word as it is entered into the list"""
-	
-	wordlen = 0
-	wordcount = 0
-	wordindex = 0
+	"""This will be used to store data about each word as it is entered into the list"""
+
 
 	def __init__(self, theword):
+		wordlen = 0
+		wordcount = 0
+		wordindex = 0
 		self.theword = theword # this should make each object equal to the word name
 
 	def incrementwordcount(self):
@@ -18,9 +18,8 @@ class wordStruct:
 		wordlen = len(self.theword)
 
 	def getwordlen(self):
-		"""do some stuff to return the wordlen"""
+		return self.wordlen
 
 	def getwordcount(self):
-		"""do some stuff to return the word count"""
-
+		return self.wordcount
 
