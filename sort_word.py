2f36025243bbcad0a719a8d48debea0f14c70eda
tim-wyatt <33131948+tim-wyatt@users.noreply.github.com>
2017-10-26 21:14:24 +0100

    commit initial main proc

---
+++ sort_word.py
@@ -0,0 +1,13 @@
+"""The aim of this project is to take a file of text and sort the words based on the last 
+letter of each word. This is the main entry point"""
+
+import sys
+
+def main ():
+	"""main entry point for the programme"""
+	pass
+
+
+__name__ == '__main__':
+	sys.exit(main())
+	
