58daffe0d898a21522b3e40a4b85c260d44fb014
tim-wyatt <33131948+tim-wyatt@users.noreply.github.com>
2017-11-02 16:12:02 +0000

    Test change from atom

--- sort_word.py
+++ sort_word.py
@@ -1,11 +1,13 @@
 
-"""The aim of this project is to take a file of text and sort the words based on the last 
+"""The aim of this project is to take a file of text and sort the words based on the last
 letter of each word. This is the main entry point"""
+
+"""this is a change"""
 
 import sys
 
 def main ():
-	
+
 	"""first thing we need to do is read in a file that is passed in as the programme is
 	executed"""
 
@@ -14,10 +16,9 @@ def parsetext ():
 
 
 """I want to use a linked list to store the words so that I can easily insert stuff into
-into the middle of the list"""	
+into the middle of the list"""
 
 
 
 __name__ == '__main__':
 	sys.exit(main())
-	
