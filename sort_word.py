d033f58c688da5146b23cad04514745b773f67b3
tim-wyatt <33131948+tim-wyatt@users.noreply.github.com>
2017-11-03 14:39:01 +0000

    started adding code to main() and also created the file to test with

--- sort_word.py
+++ sort_word.py
@@ -5,20 +5,30 @@ letter of each word. This is the main entry point"""
 
 
 import sys
+import wordcharsstruc
 
 def main ():
 
 	"""first thing we need to do is read in a file that is passed in as the programme is
 	executed"""
+	print(sys.argv[1])
+	newfile = open(sys.argv[1],'r')
+
+	#Now need to read each word into a wordStruct class and then add that to a list (linked list?)
+
+	
+
+
+
 
 
 def parsetext ():
 
 
-"""I want to use a linked list to store the words so that I can easily insert stuff into
-into the middle of the list"""
+	"""I want to use a linked list to store the words so that I can easily insert stuff into
+	into the middle of the list"""
 
 
 
-__name__ == '__main__':
-	sys.exit(main())
+if __name__ == '__main__':
+	main()
