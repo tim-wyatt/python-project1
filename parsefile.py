3c84abce3c995264d23db29e890ebd19e1854a28
tim-wyatt <33131948+tim-wyatt@users.noreply.github.com>
2017-11-03 16:03:04 +0000

    Started parsing out words from a line

--- parsefile.py
+++ parsefile.py
@@ -2,5 +2,19 @@ import sys
 
 def parsetext(newfile):
 
-    oneline = newfile.readline()
-    print (oneline)
+    lines = newfile.readline()
+    extractword(lines)
+
+
+def extractword(inline):
+    wsIndex = 0
+    chars = None
+    torange = None
+
+    torange = len(inline)  - 1
+    for chars in range (0, torange):
+        if inline[chars] == " "
+            '''we found a space, so the previous characters were a word'''
+            '''need to save the index of the last whitespace char'''
+            wsIndex = chars
+            
