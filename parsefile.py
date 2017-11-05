7bc49a03b1d42fb3aa3c0f0e51014335ac815fbe
tim-wyatt <33131948+tim-wyatt@users.noreply.github.com>
2017-11-05 16:40:12 +0000

    Changes to fix bug in sorting

--- parsefile.py
+++ parsefile.py
@@ -1,26 +1,32 @@
 def parsetext(newfile):
+    currentWordList = []
 
     while True:
         lines = newfile.readline()
         if not lines:
             break
-        extractword(lines)
+        if len(lines) > 1:
+            newWordlist = extractword(lines, currentWordList)
+            currentWordList = newWordlist
 
+    print(currentWordList)
+    print(len(currentWordList))
 
-def extractword(inline):
+def extractword(inline, wordList):
     wsIndex = 0
     chindex = 0
     torange = None
 
+
     torange = len(inline) - 1
     while chindex < torange:
-        if (inline[chindex] == " " or inline[chindex] == "."):
+        if (inline[chindex] == " " or inline[chindex] == "." or inline[chindex]=="\r"):
             '''we found a space or a full stop, so the previous characters were
             a word'''
 
             word = inline[wsIndex:chindex]
 
-            print(word)
+            addWordtoList(word, wordList)
 
             '''need to save the index of the last whitespace char'''
             wsIndex = chindex + 1
@@ -30,3 +36,26 @@ def extractword(inline):
             wsIndex += 1
         else:
             chindex += 1
+    return wordList
+
+def addWordtoList(word, wordList):
+    lastLetter = 0
+    counter = 0
+    checkStringLast = None
+    checkString = None
+
+    lastLetter = len(word) - 1
+    # Base case where list is empty
+#    if (len(wordList) == 0):
+#        wordList.append(word)
+#    else:
+        # Now we need to add each word in order of the last letter
+    while counter <= len(wordList) - 1:
+        checkString = wordList[counter]
+        checkStringLast = len(checkString) - 1
+        if(word[lastLetter] <= checkString[checkStringLast]):
+            wordList.insert(counter, word)
+            return None
+        counter += 1
+
+    wordList.append(word)
