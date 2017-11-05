8acb4277ebf1cc2c8890ce909479814a538f8243
tim-wyatt <33131948+tim-wyatt@users.noreply.github.com>
2017-11-05 16:41:32 +0000

    Added changes to make this a linked list node. Not used.

--- llnode.py
+++ llnode.py
@@ -1,26 +1,31 @@
-
-
 class node:
+    """This will be used to store data about each word as it is entered into
+    the list. I did it as an exercise in using classes - don't judge :D"""
 
-	"""This will be used to store data about each word as it is entered into the list.
-	I did it as an exercise in using classes - don't judge :D"""
+    def __init__(self, theword):
+        self.wordlen = 0
+        self.wordcount = 0
+        self.wordindex = 0
+        self.next
+        self.prev
+        self.setdata(theword)
+        self.incrementwordcount()
+        self.setwordlength()
 
-	def __init__(self, theword):
-		self.wordlen = 0
-		self.wordcount = 0
-		self.wordindex = 0
-		self.theword = theword # this should make each object equal to the word name
-		self.incrementwordcount()
-		self.setwordlength()
+    def incrementwordcount(self):
+        self.wordcount += 1
 
-	def incrementwordcount(self):
-		self.wordcount += 1
+    def setwordlength(self):
+        self.wordlen = len(self.theword)
 
-	def setwordlength(self):
-		self.wordlen = len(self.theword)
+    def getwordlen(self):
+        return self.wordlen
 
-	def getwordlen(self):
-		return self.wordlen
+    def getwordcount(self):
+        return self.wordcount
 
-	def getwordcount(self):
-		return self.wordcount
+    def setData(self, data):
+        self.theword = data
+
+    def getTheWord(self):
+        return self.theword
